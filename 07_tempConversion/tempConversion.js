aeec812ec19da0c4bdb3a0e4796fdcf072dff304
Robert Belza <belzarobert@gmail.com>
2022-05-31 17:13:57 +0200

    Adding the rest of the exercises for Fundamentals 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (tempInF) {
+  celsius = Math.round((tempInF - 32) * (5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (tempInC) {
+  fahrenheit = Math.round((tempInC * (9 / 5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
